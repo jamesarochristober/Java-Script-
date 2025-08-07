e47bdcbd14d52a85219012403640ec33a7964caa
jamesarochristober <120304321+jamesarochristober@users.noreply.github.com>
2025-08-07 15:40:05 +0530

    Add files via upload

--- day1.js
+++ day1.js
@@ -1,34 +1,74 @@
 
 
 
-   var x = 5
-   x = "hellow"
-   var x = true;
-   console.log(x)
+//    var x = 5
+//    x = "hellow"
+//    var x = true;
+//    console.log(x)
 
 
-    {
-      let  a  = 10
- }
-     console.log(10)
+//     {
+//       let  a  = 10
+//  }
+//      console.log(10)
 
 
-     let date1 = 42;
-     let data2 = "42";
-     let data3
+//      let date1 = 42;
+//      let data2 = "42";
+//      let data3
 
-     console.log(typeof date1);
-     console.log(typeof data2);
-     console.log(typeof data3);
-
-
-   let userinput = prompt("Enter Your age:")
-   console.log(typeof userinput)
+//      console.log(typeof date1);
+//      console.log(typeof data2);
+//      console.log(typeof data3);
 
 
-   const name = "John"
+//    let userinput = prompt("Enter Your age:")
+//    console.log(typeof userinput)
+
+
+//    const name = "John"
        
-         console.log(name);
+//          console.log(name);
+
+
+7-8-2025
+
+console.log(10 + 10)
+console.log(10 - 10)
+console.log(10 * 10)
+console.log(10 / 10)
+console.log(10 % 11)
+console.log(10 ** 10)
+
+let int = 10;
+    int = int++
+console.log(int)
+
+let numbers  = 20
+let numbers1 = ++numbers
+console.log(numbers, numbers1) 
+
+let a1 = 12
+let b1 = ++a1
+let c1 = ++b1
+console.log(a1,b1,c1)
+
+let d1 = 1
+let e1 = --d1
+let f1 = ++e1
+console.log(d1,e1,f1)
+
+let num1 = 20
+let num2 = num1++ + --num1
+console.log(num1,num2)
+
+
+
+
+
+
+
+
 
 
  
