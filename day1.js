71b19b799222140e0237c214bd9b85619b51e89d
jamesarochristober <120304321+jamesarochristober@users.noreply.github.com>
2025-08-07 12:28:54 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,46 @@
+
+
+
+   var x = 5
+   x = "hellow"
+   var x = true;
+   console.log(x)
+
+
+    {
+      let  a  = 10
+ }
+     console.log(10)
+
+
+     let date1 = 42;
+     let data2 = "42";
+     let data3
+
+     console.log(typeof date1);
+     console.log(typeof data2);
+     console.log(typeof data3);
+
+
+   let userinput = prompt("Enter Your age:")
+   console.log(typeof userinput)
+
+
+   const name = "John"
+       
+         console.log(name);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
