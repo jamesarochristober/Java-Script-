10af189a8b1d97a6100a95306fb383ffe08f1077
jamesarochristober <120304321+jamesarochristober@users.noreply.github.com>
2025-08-08 20:18:21 +0530

    Add files via upload

--- day1.js
+++ day1.js
@@ -31,36 +31,82 @@
 //          console.log(name);
 
 
-7-8-2025
+// 7-8-2025
 
-console.log(10 + 10)
-console.log(10 - 10)
-console.log(10 * 10)
-console.log(10 / 10)
-console.log(10 % 11)
-console.log(10 ** 10)
+// console.log(10 + 10)
+// console.log(10 - 10)
+// console.log(10 * 10)
+// console.log(10 / 10)
+// console.log(10 % 11)
+// console.log(10 ** 10)
 
-let int = 10;
-    int = int++
-console.log(int)
+// let int = 10;
+//     int = int++
+// console.log(int)
 
-let numbers  = 20
-let numbers1 = ++numbers
-console.log(numbers, numbers1) 
+// let numbers  = 20
+// let numbers1 = ++numbers
+// console.log(numbers, numbers1) 
 
-let a1 = 12
-let b1 = ++a1
-let c1 = ++b1
-console.log(a1,b1,c1)
+// let a1 = 12
+// let b1 = ++a1
+// let c1 = ++b1
+// console.log(a1,b1,c1)
 
-let d1 = 1
-let e1 = --d1
-let f1 = ++e1
-console.log(d1,e1,f1)
+// let d1 = 1
+// let e1 = --d1
+// let f1 = ++e1
+// console.log(d1,e1,f1)
 
-let num1 = 20
-let num2 = num1++ + --num1
-console.log(num1,num2)
+// let num1 = 20
+// let num2 = num1++ + --num1
+// console.log(num1,num2)
+
+  
+8-8-2025
+
+    let x  = 5
+    let y  = x++ + ++x
+    console.log(x,y)
+
+    let total  = 50
+        total += 30
+        total /= 4
+    console.log(total)
+
+    console.log(10=="10")
+    console.log(10==="10")
+
+    // let age = 25
+    // let experience = 3
+
+    console.log(typeof(5 + "5"));
+
+    console.log(Boolean(0));
+    console.log(Boolean("false"));
+    console.log(Boolean([]));
+
+    // result = int("123") + 10
+    // console.log(result)
+
+    console.log(Boolean({}));
+    console.log(Boolean(null));
+
+    let greeting = "Hello" + " " + "Students";
+    console.log(greeting);
+
+    let a = "10";
+    let b = 10;
+    console.log(a + b);
+    console.log(Number(a) + b);
+
+    
+   
+     
+
+ 
+
+
 
 
 
