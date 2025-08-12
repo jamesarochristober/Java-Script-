95048a98530ac5c897f4dca7cdc5e3e3f305e23d
jamesarochristober <120304321+jamesarochristober@users.noreply.github.com>
2025-08-12 20:32:51 +0530

    Add files via upload

--- day1.js
+++ day1.js
@@ -125,85 +125,157 @@
 //     console.log(Number(a) + b);
 
 
- 11-8-2025
+//  11-8-2025
+
+//  // - 1
+
+//  let Number = prompt("Enter Your Number")
+
+//  if (Number > 0) {
+//     console.log("Positive Number");
+
+//  } else if ( Number < 0) {
+//      console.log("Negative Number");
+
+//  } else {
+//     console.log("Zero");
+//  }
+
+//   // - 2
+
+//    let monthNumber = 7;
+
+//   switch (monthNumber) {
+
+//     case "1":console.log("January"); break;
+        
+//     case "2": console.log("February"); break;
+        
+//     case "3": console.log("March"); break;
+        
+//     case "4": console.log("April"); break;
+
+//     case "5":console.log("May"); break;
+    
+//     case "6": console.log("June"); break;
+       
+//     case 7:console.log("July"); break;
+        
+//     case 8:  console.log("August"); break;
+      
+//     case 9:console.log("September"); break;
+        
+//     case "10": console.log("October"); break;
+        
+//     case "11": console.log("November"); break;
+          
+//     case "12": console.log("December"); break;
+        
+//     default:
+//         console.log("Invalid month number");
+// }
+
+
+
+// // - 3
+
+// let number = 7;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+// // - 4
+
+// let colors = ["red", "green", "blue", "yellow", "Black"];
+
+// for (let color of colors) {
+//     console.log(color.toUpperCase());
+// }
+
+// // - 5
+
+// let car = {
+//   brand: "Mercedes-Benz",
+//   model: "S-Class",
+//   year: 2025
+// };
+
+// for (let property in car) {
+//   console.log(property + ": " + car[property]);
+// }
+
+
+ 12-8-2025
 
  // - 1
 
- let Number = prompt("Enter Your Number")
-
- if (Number > 0) {
-    console.log("Positive Number");
-
- } else if ( Number < 0) {
-     console.log("Negative Number");
-
- } else {
-    console.log("Zero");
- }
-
-  // - 2
-
-   let monthNumber = 7;
-
-  switch (monthNumber) {
-
-    case "1":console.log("January"); break;
-        
-    case "2": console.log("February"); break;
-        
-    case "3": console.log("March"); break;
-        
-    case "4": console.log("April"); break;
-
-    case "5":console.log("May"); break;
-    
-    case "6": console.log("June"); break;
-       
-    case 7:console.log("July"); break;
-        
-    case 8:  console.log("August"); break;
-      
-    case 9:console.log("September"); break;
-        
-    case "10": console.log("October"); break;
-        
-    case "11": console.log("November"); break;
-          
-    case "12": console.log("December"); break;
-        
-    default:
-        console.log("Invalid month number");
+ function isMagicNumber(num) {
+  if (num % 3 === 0 && num % 7 === 0) {
+    console.log("* It's magic!");
+  } else {
+    console.log("Not magic");
+  }
 }
 
+isMagicNumber(21);
+isMagicNumber(10);
 
+// - 2
+
+let shoutName = function(name) {
+    console.log(name.toUpperCase() + "!!!");
+};
+
+shoutName("chris");
 
 // - 3
 
-let number = 7;
+const tellFortune = (name, place = "Unknown") => {
+    console.log(`Dear ${name}, you will travel to ${place} soon!`);
+};
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`${number} x ${i} = ${number * i}`);
-}
+tellFortune("Chris", "London");
+tellFortune("James");
 
 // - 4
 
-let colors = ["red", "green", "blue", "yellow", "Black"];
+function scopeDetective() {
+    if (true) {
+        var varVariable = "I am var!";
+        let letVariable = "I am let!";
+        const constVariable = "I am const!";
 
-for (let color of colors) {
-    console.log(color.toUpperCase());
+        console.log("Inside block - var:", varVariable);
+        console.log("Inside block - let:", letVariable);
+        console.log("Inside block - const:", constVariable); 
+    }
+
+    console.log("Outside block - var:", varVariable); 
+
+    try {
+        console.log("Outside block - let:", letVariable); 
+    } catch (error) {
+        console.log("Outside block - let: Not accessible (" + error.message + ")");
+    }
+
+    try {
+        console.log("Outside block - const:", constVariable);
+    } catch (error) {
+        console.log("Outside block - const: Not accessible (" + error.message + ")");
+    }
 }
+
+scopeDetective();
 
 // - 5
 
-let car = {
-  brand: "Mercedes-Benz",
-  model: "S-Class",
-  year: 2025
-};
+const sumNumbers = (a, b) => a + b;
 
-for (let property in car) {
-  console.log(property + ": " + car[property]);
-}
+console.log(sumNumbers(1, 6));
+
+
+ 
 
 
 
@@ -214,7 +286,7 @@ for (let property in car) {
 
 
 
-
+   
      
 
  
